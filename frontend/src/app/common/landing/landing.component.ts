4bb1c1e57dbd8a862e3b6d29e07a9321ee619782
Waltter2211 <110539577+Waltter2211@users.noreply.github.com>
2024-10-15 19:39:48 +0300

    made minor fixes to styling

--- frontend/src/app/common/landing/landing.component.ts
+++ frontend/src/app/common/landing/landing.component.ts
@@ -1,15 +1,14 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [MatCardModule, MatFormFieldModule, MatInputModule],
+  imports: [MatCardModule, MatFormFieldModule, MatInputModule, RouterLink],
   templateUrl: './landing.component.html',
-  styleUrl: './landing.component.css'
+  styleUrl: './landing.component.css',
 })
-export class LandingComponent {
-
-}
+export class LandingComponent {}
