a005667248eb69e66c5642d725c5f0e53e66a618
Waltter2211 <110539577+Waltter2211@users.noreply.github.com>
2024-10-16 05:54:54 +0300

    created animations for transitions between landing page, login page and register page

--- frontend/src/app/common/login/login.component.ts
+++ frontend/src/app/common/login/login.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatFormFieldModule, MatInputModule, RouterLink],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
-export class LoginComponent {
-
-}
+export class LoginComponent {}
--- frontend/src/app/common/register/register.component.ts
+++ frontend/src/app/common/register/register.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatFormFieldModule, MatInputModule, RouterLink],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.css',
 })
-export class RegisterComponent {
-
-}
+export class RegisterComponent {}
